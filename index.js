0253a413441e78a37e14d67e7b58b5d927fbd8b2
James Robson <jrobson@turnitin.com>
2017-02-09 11:28:33 +0000

    Tidied up an added comments

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+/* global newrelic */
+
+let _ = require('underscore');
+
 module.exports = {
-  trackBehaviour = function(name, attrs) {
-    if (!name) {
-      return;
-    }
+    trackBehaviour: function (name, attrs) {
+        if (!name) {
+            return;
+        }
 
-    attrs = _.defaults({}, attrs, {
-      role: window.curr_role,
-      lang: window.curr_lang,
-      hostname: window.location.hostname,
-      mode: window.curr_mode
-    });
+        attrs = _.defaults({}, attrs, {
+            role: window.curr_role,
+            lang: window.curr_lang,
+            hostname: window.location.hostname,
+            mode: window.curr_mode
+        });
 
-    if (window.newrelic) {
-      newrelic.addPageAction(name, attrs)
+        // The newrelic JS snippet should be included on relevant hosts (i.e.
+        // non dev hosts) above the first script tag inside the HEAD tag, or
+        // at the bottom of the HEAD tag.
+        if (window.newrelic) {
+            newrelic.addPageAction(name, attrs);
+        }
     }
-  }
 };
